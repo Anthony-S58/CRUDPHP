ce2b2d4dd79f9a1a876ad0db603ce6018a9afa7e
Anthony-S58 <tonysim01@hotmail.com>
2021-06-07 09:29:44 +0200

    modif2

--- index.php
+++ index.php
@@ -5,20 +5,53 @@ require_once('connexion.php');
 $sql='SELECT * from crud';
 $query = $db->prepare($sql);
 $query->execute(); 
-$result = $query->fetchAll(PDO::FETCH_ASSOC); //tableau associatif
+$result = $query->fetchAll(PDO::FETCH_ASSOC); 
 
-var_dump($result);
+// var_dump($result);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>projet</title>
 </head>
 <body>
+
+<table>
+
+    <thead>
+
+        <th>PROJET</th>
+        <th>IMAGE</th>
+        <th>DESCRIPTION</th>
+    
+    </thead>
+
+    <tbody>
+
+        <?php
+        foreach ($result as $projet) {
+        ?>
+            <tr>
+            
+                <td><?php echo $projet['Nom'];?></td>    
+                <td><?= $projet['Image'];?></td>
+                <td><?= $projet['Description'];?></td>
+                <td><a href="supprimer.php?ID=<?= $projet['ID'];?>">Supprimer</a> </td>
+
+            </tr>
+
+        <?php }
+        ?>
+
+    </tbody>
+
+
+
+</table>
     
 </body>
 </html>
